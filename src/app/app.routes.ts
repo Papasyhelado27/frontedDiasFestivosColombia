9e19fed82b47f959331f2e0eb19c9248539c3001
Juan Pablo Salazar <140007704+Papasyhelado27@users.noreply.github.com>
2025-05-31 17:05:46 -0500

    Update app.routes.ts
    se saca el componente grupo de  la ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from '../features/componentes/inicio/inicio.component';
 import { SeleccionComponent } from '../features/componentes/seleccion/seleccion.component';
 import { CampeonatoComponent } from '../features/componentes/campeonato/campeonato.component';
-import { GrupoComponent } from '../features/componentes/grupo/grupo.component';
+
 import { FestivosComponent } from '../features/componentes/festivos/festivos.component';
 
 export const routes: Routes = [
@@ -10,6 +10,6 @@ export const routes: Routes = [
     { path: "inicio", component: InicioComponent },
     { path: "selecciones", component: SeleccionComponent },
     { path: "campeonatos", component: CampeonatoComponent },
-    { path: "grupos", component: GrupoComponent },
+    
     { path: "festivos", component: FestivosComponent }
 ];
