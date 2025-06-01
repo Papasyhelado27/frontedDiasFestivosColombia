529009c22d1463505f6226e8ed083e66894f924c
Juan Pablo Salazar <140007704+Papasyhelado27@users.noreply.github.com>
2025-06-01 15:41:34 -0500

    Update app.routes.ts
    se quitan rutas que no se usaran en el fronted

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from '../features/componentes/inicio/inicio.component';
 import { SeleccionComponent } from '../features/componentes/seleccion/seleccion.component';
-import { CampeonatoComponent } from '../features/componentes/campeonato/campeonato.component';
+
 
 import { FestivosComponent } from '../features/componentes/festivos/festivos.component';
 
@@ -9,7 +9,7 @@ export const routes: Routes = [
     { path: "", redirectTo: "inicio", pathMatch:"full" },
     { path: "inicio", component: InicioComponent },
     { path: "selecciones", component: SeleccionComponent },
-    { path: "campeonatos", component: CampeonatoComponent },
+   
     
     { path: "festivos", component: FestivosComponent }
 ];
